ce80c280ef2c06d1e9730eada83e5925b3608a72
victoriaonimisi <97122658+victoriaonimisi@users.noreply.github.com>
2022-03-19 22:56:57 -0500

    Add files via upload

---
+++ before-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# will install the dependencies to run the applications.
+
+#install docker
+sudo apt update
+yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+yes | sudo apt install docker-ce
+
+# update user to not require sudo for docker commands
+sudo usermod -aG docker ${USER}
+su - ${USER}
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
